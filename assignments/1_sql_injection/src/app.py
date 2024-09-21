200f52afc4469ff7c196ac10ba27ab114b88a787
SimonWilmotsIIW <simon.wilmots@student.uhasselt.be>
2024-09-21 21:55:32 +0200

    Assignment 1: SQL injection prone app done

--- assignments/1_sql_injection/src/app.py
+++ assignments/1_sql_injection/src/app.py
@@ -30,10 +30,12 @@ def prefill():
 
     astronauts = [
         ('Neil Armstrong', 'Apollo 11', 'Commander', 'First person to walk on the moon', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Neil_Armstrong_pose.jpg/1200px-Neil_Armstrong_pose.jpg'),
-        ('Howard Wolowitz', 'Apollo 11', 'Payload Specialist', 'First NASA Engineer in space', 'https://yt3.googleusercontent.com/ytc/AIdro_nrUCA3ICcPgx6_8J_rdEFlh8MU4Q4HaoweIZRHiTzrLg=s900-c-k-c0x00ffffff-no-rj'),
+        ('Howard Wolowitz', 'Apollo 11', 'Payload Specialist', 'Installation of the Wolowitz Waste Deposit System', 'https://yt3.googleusercontent.com/ytc/AIdro_nrUCA3ICcPgx6_8J_rdEFlh8MU4Q4HaoweIZRHiTzrLg=s900-c-k-c0x00ffffff-no-rj'),
         ('Yuri Gagarin', 'Vostok 1', 'Pilot', 'First human in space', 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Yuri_Gagarin_%281961%29_-_Restoration.jpg'),
         ('Sally Ride', 'STS-7', 'Mission Specialist', 'First American woman in space', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Sally_Ride_%281984%29.jpg/1200px-Sally_Ride_%281984%29.jpg'),
-        ('Simon Wilmots', 'CRS-23', 'Mission Specialist', 'First astronaut from Hasselt University', 'https://media.licdn.com/dms/image/v2/D4E03AQEvqJVZ042IpQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1720721310891?e=2147483647&v=beta&t=JKMiVPwx0zuFO2F0QXngejAL3P1pOzm-2wtO4J-sfC8'),
+        ('Dirk Frimout', 'STS-45 ', 'Payload Specialist', 'First Belgian astronaut', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Frimout.jpg/440px-Frimout.jpg'),
+        ('Frank De Winne', 'ISS Expedition 21', 'Commander', 'First Belgian ESA astronaut', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Frank_De_Winne_2009.jpg/440px-Frank_De_Winne_2009.jpg'),
+        ('Simon Wilmots', 'OSCAR', 'Wannabe astronaut', 'First astronaut from Hasselt University', 'https://media.licdn.com/dms/image/v2/D4E03AQEvqJVZ042IpQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1720721310891?e=2147483647&v=beta&t=JKMiVPwx0zuFO2F0QXngejAL3P1pOzm-2wtO4J-sfC8'),
 
     ]
 
@@ -67,7 +69,9 @@ def search():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     name = request.form['query']
-    query = f"SELECT * FROM astronauts WHERE name LIKE '%{name}%'"
+    # SQL injection is possible here with:
+    # query = f"SELECT * FROM astronauts WHERE name ='{name}'"
+    query = f"SELECT * FROM astronauts WHERE name ='{name}'"
     astronauts = cursor.execute(query)
     return render_template('index.html', astronauts=astronauts)
 
@@ -86,7 +90,7 @@ def home():
 
         cursor.execute('''
             INSERT INTO astronauts (name, mission, rank, achievements, picture) 
-            VALUES (?, ?, ?, ?)
+            VALUES (?, ?, ?, ?, ?)
         ''', (name, mission, rank, achievements, picture))
         conn.commit()
 
@@ -97,7 +101,7 @@ def home():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
 
-    cursor.execute('SELECT name, mission, rank, achievements, picture FROM astronauts')
+    cursor.execute('SELECT * FROM astronauts')
     astronauts = cursor.fetchall()
 
     conn.close()
